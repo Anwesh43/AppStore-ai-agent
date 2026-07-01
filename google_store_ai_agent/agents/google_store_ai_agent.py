bd7981db78c5e5d9a16299033720f12b24fcd0af
Anwesh43 <anweshthecool0@gmail.com>
2026-07-01 22:33:14 +0530

    modifying agent and prompt

--- google_store_ai_agent/agents/google_store_ai_agent.py
+++ google_store_ai_agent/agents/google_store_ai_agent.py
@@ -1,4 +1,4 @@
-from tools.serp_tool import getProductDescriptionForPlayStoreApp, getPlayStoreApps
+from tools.serp_tool import getProductDescriptionForPlayStoreApp, getPlayStoreApps, getAppleStoreApps, getProductStoreDescriptionForAppleStoreApp
 from prompts.google_store_prompts import SYSTEM_PROMPT
 from pydantic_ai import Agent 
 from dotenv import load_dotenv
@@ -7,7 +7,7 @@ load_dotenv()
 
 agent = Agent(
     system_prompt = SYSTEM_PROMPT,
-    tools = [getProductDescriptionForPlayStoreApp, getPlayStoreApps],
+    tools = [getProductDescriptionForPlayStoreApp, getPlayStoreApps, getAppleStoreApps, getProductStoreDescriptionForAppleStoreApp],
     model = "openai-chat:gpt-5.2"
 )
 
